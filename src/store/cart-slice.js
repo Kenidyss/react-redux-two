6a353ec86ab659657ac3377272de0844fbd4fd60
Kenidy Santana <kenidys@gmail.com>
2022-02-06 06:34:28 -0300

    wire data with redux

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-createSlice({
+const cartSlice = createSlice({
   name: "cart",
   initialState: {
     items: [],
@@ -9,7 +9,9 @@ createSlice({
   reducers: {
     addToCart(state, action) {
       const newItem = action.payload;
-      existingItem = state.items.find((item) => item.id === newItem);
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
+
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
@@ -19,11 +21,22 @@ createSlice({
           name: newItem.title,
         });
       } else {
-        existingItem.quantity++, (existingItem.totalPrice += newItem.price);
+        existingItem.quantity++;
+        existingItem.totalPrice += newItem.price;
       }
     },
     removeItemFromCart(state, action) {
-      id = action.payload;
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
     },
   },
 });
+export const cartActions = cartSlice.actions;
+export default cartSlice;
